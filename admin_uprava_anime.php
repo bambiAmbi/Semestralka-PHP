8c179d8b803b755379c46b596530853da43578e4
bambiAmbi <104720289+bambiAmbi@users.noreply.github.com>
2023-06-20 11:22:55 +0200

    validace úpravy anime + mazání obrázku

--- admin_uprava_anime.php
+++ admin_uprava_anime.php
@@ -2,6 +2,13 @@
 
 session_start();
 $id_uzivatele = htmlspecialchars($_SESSION["id_uzivatele"]);
+$validni_post = false;
+$validni_jmeno = true;
+$validni_jp_jmeno = true;
+$validni_pocet_epizod = true;
+$validni_pocet_serii = true;
+$validni_popis_anime = true;
+
 
 if($id_uzivatele != 14) {
     header("Location: index2.html");
@@ -16,6 +23,92 @@ if(isset($_GET['id'])) {
     $vysledek_anime = $db->query($sql_anime);
     $vybrane_anime = $vysledek_anime->fetch(PDO::FETCH_ASSOC);
 
+}
+
+if(isset($_POST['submit'])){
+    if (empty($_POST["nazev_eng"])) {
+        $validni_jmeno = false;
+    }
+
+    if (empty($_POST["nazev_jp"])) {
+        $validni_jp_jmeno = false;
+    }
+
+    if (empty($_POST["pocet_ep"])) {
+        $validni_pocet_epizod = false;
+    } else if($_POST["pocet_ep"] <= 0) {
+        $validni_pocet_epizod = false;
+    }
+
+    if (empty($_POST["pocet_s"])) {
+        $validni_pocet_serii = false;
+    } else if($_POST["pocet_s"] <= 0) {
+        $validni_pocet_serii = false;
+    }
+
+    if (empty($_POST["popis"])) {
+        $validni_popis_anime = false;
+    }
+
+
+    if($validni_jmeno && $validni_jp_jmeno && $validni_pocet_epizod && $validni_pocet_serii && $validni_popis_anime){
+        $validni_post = true;
+    }
+    if($validni_post) {
+        $id = htmlspecialchars($_GET['id']);
+        $sql_puvodni_soubor="SELECT OBRAZEK_CESTA FROM ANIME WHERE ID = $id";
+        $vysledek_souboru = $db->query($sql_puvodni_soubor);
+        $vybrana_cesta = $vysledek_souboru->fetch(PDO::FETCH_ASSOC);
+        $stara_cesta = $vybrana_cesta['OBRAZEK_CESTA'];
+
+        unlink($stara_cesta);
+
+
+        $nazev_souboru = $_FILES['files']['name'];
+        $cilovy_soubor = './uploads/'.$nazev_souboru[0];
+        $typ_souboru = pathinfo(
+            $cilovy_soubor, PATHINFO_EXTENSION);
+        $typ_souboru = strtolower($typ_souboru);
+        $cilovy_soubor = './uploads/'.$id.'.'.$typ_souboru;
+
+        // Upload file
+        move_uploaded_file($_FILES['files']['tmp_name'][0], $cilovy_soubor);
+
+
+        $data = [
+            'NAZEV_ENG' => htmlspecialchars($_POST["nazev_eng"]),
+            'NAZEV_JP' => htmlspecialchars($_POST["nazev_jp"]),
+            'POCET_EPIZOD' => htmlspecialchars($_POST["pocet_ep"]),
+            'POCET_SERII' => htmlspecialchars($_POST["pocet_s"]),
+            'POPIS' => htmlspecialchars($_POST["popis"]),
+            'OBRAZEK_CESTA' => htmlspecialchars($cilovy_soubor)
+
+        ];
+
+        $sql = "UPDATE ANIME SET 
+                 NAZEV_ENG = :NAZEV_ENG, 
+                 NAZEV_JP = :NAZEV_JP,
+                 POCET_EPIZOD = :POCET_EPIZOD,
+                 POCET_SERII = :POCET_SERII,
+                 POPIS = :POPIS,
+                 OBRAZEK_CESTA = :OBRAZEK_CESTA
+                 WHERE ID LIKE $id";
+        $statement = $db->prepare($sql);
+        $statement->execute($data);
+
+
+        try {
+            $statement->execute($data);
+            header("Location: admin_uspesne.html");
+            exit;
+        } catch (PDOException $e) {
+            if ($e->errorInfo[1] == 1062) {
+                echo "Toto anime je již zadané.";
+            } else {
+                $db->errorInfo();
+            }
+        }
+    }
 }
 ?>
 
@@ -104,26 +197,39 @@ if(isset($_GET['id'])) {
 
 
           <?php if (isset($_GET['id'])): ?>
-          <form method="post" action="upravit_anime.php">
+          <form method='post' action=''
+                enctype='multipart/form-data'>
               <div class="pridani">
                   <div>
                       <label for="nazev_eng">Název v angličtině</label>
                       <input type="text" name="nazev_eng" id="nazev_eng" value="<?= htmlspecialchars($vybrane_anime['NAZEV_ENG'] ?? "")?>">
+                      <?php if(!$validni_jmeno): ?>
+                          <em>Musíte zadat anglický název anime</em>
+                      <?php endif; ?>
                   </div>
 
                   <div>
                       <label for="nazev_jp">Název v japonštině</label>
                       <input type="text" name="nazev_jp" id="nazev_jp" value="<?= htmlspecialchars($vybrane_anime['NAZEV_JP'] ?? "")?>">
+                      <?php if(!$validni_jp_jmeno): ?>
+                          <em>Musíte zadat japonský název anime</em>
+                      <?php endif; ?>
                   </div>
 
                   <div>
                       <label for="pocet_ep">Počet epizod</label>
                       <input type="number" name="pocet_ep" id="pocet_ep" value="<?= htmlspecialchars($vybrane_anime['POCET_EPIZOD'] ?? "")?>">
+                      <?php if(!$validni_pocet_epizod): ?>
+                          <em>Počet epizod musí být větší než 0</em>
+                      <?php endif; ?>
                   </div>
 
                   <div>
                       <label for="pocet_s">Počet sezon</label>
                       <input type="number" name="pocet_s" id="pocet_s" value="<?= htmlspecialchars($vybrane_anime['POCET_SERII'] ?? "")?>">
+                      <?php if(!$validni_pocet_serii): ?>
+                          <em>Počet sérií musí být větší než 0</em>
+                      <?php endif; ?>
                   </div>
 
                   <div>
@@ -131,6 +237,9 @@ if(isset($_GET['id'])) {
                       <br>
                       <textarea id="popis" name="popis" rows="6" cols="50"><?= htmlspecialchars($vybrane_anime['POPIS'] ?? "")?></textarea>
                       <br/>
+                      <?php if(!$validni_popis_anime): ?>
+                          <em>Musíte zadat popis anime</em>
+                      <?php endif; ?>
                   </div>
 
                   <div>
@@ -138,10 +247,9 @@ if(isset($_GET['id'])) {
                   </div>
 
                   <div>
-                      <label for="obrazek">Náhledový obrázek</label>
-                      <input type="file" name="obrazek" id="obrazek" accept="image/png, image/jpeg, image/jpg"  value="<?= htmlspecialchars($vybrane_anime['OBRAZEK'] ?? "")?>">
-
-                      <button>Přidat</button>
+                      <label for="image">Název obrázku</label>
+                      <input type='file' name='files[]' accept="image/png, image/jpeg" required/>
+                      <input type='submit' value='Přidat' name='submit' />
                   </div>
               </div>
           </form>
